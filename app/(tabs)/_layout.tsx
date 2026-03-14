d9610391ae44760195f7b588f1e0cc85e18375db
Srnishappy <suranatsuwannarat@gmail.com>
2026-03-14 14:28:33 +0700

    feat: add dark mode using ThemeContext and AsyncStorage

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,17 @@
+import { useTheme } from '@/hooks/useTheme';
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 const TabLayout = () => {
+  const { colors } = useTheme();
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: 'red',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
         tabBarStyle: {
-          backgroundColor: '#1e293b',
+          backgroundColor: colors.surface,
           borderTopWidth: 1,
-          borderTopColor: 'yellow',
+          borderTopColor: colors.border,
           height: 90,
           paddingBottom: 30,
           paddingTop: 10,
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useTheme } from '@/hooks/useTheme';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
     <View style={styles.container}>
       <Text style={styles.content}>test</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text> toggle the mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
